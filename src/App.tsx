85eef4e0fcc091e76c2c4e1aa6b4976df0ee10f5
thaynar96 <thaynarbo@gmail.com>
2021-09-28 11:29:20 -0300

    removing comments andd changing App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Route path='/' exact component={Home} />
+			<Route path='/rooms/new' component={NewRoom} />
+		</BrowserRouter>
+	);
 }
-
-export default App;
